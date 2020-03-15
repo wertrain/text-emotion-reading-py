54d6da5012b0eceeb60e356fbde71bb1093663e7
wertrain <wertrain@gmail.com>
2020-03-15 15:21:42 +0900

    警告が出ていたのを修正

--- boot_vscode_on_venv.bat
+++ boot_vscode_on_venv.bat
@@ -1,7 +1,7 @@
+@echo off
 :: 現在のディレクトリに移動
 cd /d %~dp0
 :: virtualenv を起動
-call venv\Scripts\activate
+call "venv\Scripts\activate"
 :: Visual Studio Code を起動
-:: ここから起動することで Visual Studio Code 内でデバッグ実行可能になる
 code
