a438521314139d620bbdf229c6c8d13c603bb099
wertrain <wertrain@gmail.com>
2020-03-28 11:50:51 +0900

    ドキュメントコメントの追加

--- emotion_db.py
+++ emotion_db.py
@@ -3,6 +3,11 @@ import csv
 import pickle
 
 def create_emotion_score_dict():
+    """カテゴリをキーとした感情スコア辞書を作成する
+    
+    Returns:
+        dictionary: 感情スコア辞書
+    """
     score_dict = {}
     with open("emotion_score_asynset.csv", "r") as f:
         data = csv.reader(f)
@@ -13,6 +18,11 @@ def create_emotion_score_dict():
     return score_dict
 
 def create_emotion_dict():
+    """感情を表す語をキーとしたカテゴリ辞書を作成する
+    
+    Returns:
+        dictionary: カテゴリ辞書
+    """
     tree = ET.parse('jpn-asynset.xml')
     root = tree.getroot()
     emotion_dict = {}
@@ -26,10 +36,14 @@ def create_emotion_dict():
     return emotion_dict
 
 def load_emotion_dict():
+    """感情を表す語をキーとしたカテゴリ辞書をファイルから読み込む
+    """
     with open("emotion_dict.bin", mode='rb') as f:
         data = pickle.load(f)
         return data
 
 def save_emotion_dict():
+    """感情を表す語をキーとしたカテゴリ辞書をファイルに出力
+    """
     with open("emotion_dict.bin", mode='wb') as f:
         pickle.dump(create_emotion_dict(),f)
