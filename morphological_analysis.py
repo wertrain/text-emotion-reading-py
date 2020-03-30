e19e8d5f6df85b9ae43a6a65c690cd96ca73a6b9
wertrain <wertrain@gmail.com>
2020-03-30 23:32:09 +0900

    不要ファイルの削除

--- morphological_analysis.py
+++
@@ -1,9 +0,0 @@
-from janome.tokenizer import Tokenizer
-
-t = Tokenizer()
-s = '''
-新しい人達に接して見ても、一週間たつかたたない内に、彼は又しても底知れぬ倦怠けんたいの中に沈み込んで了うのでした。
-そうして、東栄館に移って十日ばかりたったある日のことです。退屈の余り、彼はふと妙な事を考えつきました。
-'''
-for token in t.tokenize(s):
-    print(token)
